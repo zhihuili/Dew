269abc7ee810b5359a0185ef056764275cc1f6d1
wwqwccjky <wwqwcc@126.com>
2014-11-18 16:38:03 +0800

    add cpu and load diagnosis

--- app.appdiagnosis/src/main/java/com/intel/sto/bigdata/app/appdiagnosis/strategy/CpuUtilityDiagnosisStrategy.java
+++ app.appdiagnosis/src/main/java/com/intel/sto/bigdata/app/appdiagnosis/strategy/CpuUtilityDiagnosisStrategy.java
@@ -1,9 +1,16 @@
 package com.intel.sto.bigdata.app.appdiagnosis.strategy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.intel.sto.bigdata.app.appdiagnosis.DiagnosisContext;
 import com.intel.sto.bigdata.app.appdiagnosis.DiagnosisResult;
+import com.intel.sto.bigdata.app.appdiagnosis.DiagnosisResult.Level;
+import com.intel.sto.bigdata.app.appdiagnosis.util.DstatUtil;
+
+;
 
 /**
  * Which nodes' max cpu utility (user + sys) is low? 50% high, 70% middle, 90$ low
@@ -13,7 +20,40 @@ public class CpuUtilityDiagnosisStrategy implements DiagnosisStrategy {
 
   @Override
   public List<DiagnosisResult> diagnose(DiagnosisContext context) {
-    return null;
+    List<DiagnosisResult> cpuDiagnosisResult = new ArrayList<DiagnosisResult>();
+    Map<String, List<Map<String, List<List<String>>>>> dataSet = context.getPerformanceData();
+    for (String hostName : dataSet.keySet()) {
+      DiagnosisResult tmpResult = new DiagnosisResult();
+
+      tmpResult.setDiagnosisName("CPU");
+      tmpResult.setHostName(hostName);
+      List<Map<String, List<List<String>>>> hostDataSet = dataSet.get(hostName);
+      double maxCpuUtility = 0.0;
+      for (int i = 0; i < hostDataSet.size(); i++) {
+        List<String> dataRecord = new ArrayList<String>();
+        dataRecord = hostDataSet.get(i).get(null).get(0);
+        Map<String, Double> parseResult = DstatUtil.parseDstat(dataRecord);
+        double cpuUtility = parseResult.get("usr") + parseResult.get("sys");
+        maxCpuUtility = (maxCpuUtility < cpuUtility) ? cpuUtility : maxCpuUtility;
+      }
+
+      if (maxCpuUtility > 0.9) {
+        tmpResult.setLevel(Level.high);
+        tmpResult.setDescribe("cpu utility is high");
+        tmpResult.setAdvice("null");
+      } else if (maxCpuUtility > 0.7) {
+        tmpResult.setLevel(Level.middle);
+        tmpResult.setDescribe("cpu utility is middle");
+        tmpResult.setAdvice("allocate more task on cpu properly");
+      } else {
+        tmpResult.setLevel(Level.low);
+        tmpResult.setDescribe("cpu utility is low");
+        tmpResult.setAdvice("check the calculate process");
+      }
+
+      cpuDiagnosisResult.add(tmpResult);
+    }
+    return cpuDiagnosisResult;
   }
 
 }
--- app.appdiagnosis/src/main/java/com/intel/sto/bigdata/app/appdiagnosis/util/DstatUtil.java
+++ app.appdiagnosis/src/main/java/com/intel/sto/bigdata/app/appdiagnosis/util/DstatUtil.java
@@ -1,9 +1,13 @@
 package com.intel.sto.bigdata.app.appdiagnosis.util;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DstatUtil {
+
+  public static String metricsHead =
+      "used,buffer,cached,free,ioread,iowrit,usr,sys,idl,wai,hiq,siq,eth0recv,eth0send,eth1recv,eth1send,totalrecv,totalsend,diskread,diskwrit";
 
   private DstatUtil() {
   }
@@ -14,7 +18,14 @@ public class DstatUtil {
    * @return
    */
   public static Map<String, Double> parseDstat(List<String> line) {
-    // TODO
-    return null;
+    Map<String, Double> parseResult = new HashMap<String, Double>();
+
+    String head[] = metricsHead.split(",");
+
+    for (int i = 0; i < line.size(); i++) {
+      parseResult.put(head[i], Double.valueOf(line.get(i)));
+    }
+
+    return parseResult;
   }
 }
