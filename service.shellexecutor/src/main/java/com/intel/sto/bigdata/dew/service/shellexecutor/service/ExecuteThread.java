0842acceb3c815c0597c35d58f6547ddd06c2378
Li Zhihui <zhihui.li@intel.com>
2014-12-17 09:36:03 +0800

    Add service.shellexecutor to run executable command in Agent

---
+++ service.shellexecutor/src/main/java/com/intel/sto/bigdata/dew/service/shellexecutor/service/ExecuteThread.java
@@ -0,0 +1,75 @@
+package com.intel.sto.bigdata.dew.service.shellexecutor.service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.intel.sto.bigdata.dew.http.client.HttpSimpleClient;
+import com.intel.sto.bigdata.dew.http.client.HttpStreamClient;
+import com.intel.sto.bigdata.dew.service.shellexecutor.message.ExecuteRequest;
+
+public class ExecuteThread extends Thread {
+  ExecuteRequest request;
+
+  public ExecuteThread(ExecuteRequest request) {
+    this.request = request;
+  }
+
+  @Override
+  public void run() {
+    Runtime runtime = Runtime.getRuntime();
+    BufferedReader br = null;
+    FileWriter fw = null;
+    try {
+      Process process =
+          runtime.exec(request.getCommand(), request.getEnvp(), new File(request.getDirectory()));
+      br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      fw = new FileWriter("/tmp/" + request.getId());
+      String line;
+      while ((line = br.readLine()) != null) {
+        fw.write(line + System.getProperty("line.separator"));
+      }
+      int exitValue = process.exitValue();
+      if (request.getStatusUrl() != null && !request.getStatusUrl().trim().equals("")) {
+        Map<String, String> status = new HashMap<String, String>();
+        status.put("id", request.getId());
+        if (exitValue == 0) {
+          status.put("status", "failure");
+        } else {
+          status.put("status", "success");
+        }
+        HttpSimpleClient.get(request.getStatusUrl(), status);
+        if (request.getLogUrl() != null) {
+          Map<String, String> para = new HashMap<String, String>();
+          para.put("id", request.getId());
+          InputStream is = new FileInputStream("/tmp/" + request.getId());
+          HttpStreamClient.post(request.getLogUrl(), para, is);
+        }
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      if (fw != null) {
+        try {
+          fw.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      if (br != null) {
+        try {
+          br.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+}
