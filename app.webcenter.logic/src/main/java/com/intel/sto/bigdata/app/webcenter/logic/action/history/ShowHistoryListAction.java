ea2020b02e7b5389dab475d158c74750efa32e0b
Li Zhihui <zhihui.li@intel.com>
2015-01-12 10:17:36 +0800

    Add Utils class for calling SparkPowerMeter

--- app.webcenter.logic/src/main/java/com/intel/sto/bigdata/app/webcenter/logic/action/history/ShowHistoryListAction.java
+++ app.webcenter.logic/src/main/java/com/intel/sto/bigdata/app/webcenter/logic/action/history/ShowHistoryListAction.java
@@ -3,12 +3,8 @@ package com.intel.sto.bigdata.app.webcenter.logic.action.history;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Properties;
 
-import org.apache.struts2.ServletActionContext;
-
-import com.intel.sto.bigdata.app.sparkperformance.Constants;
-import com.intel.sto.bigdata.app.sparkperformance.Util;
+import com.intel.sto.bigdata.app.webcenter.logic.WebCenterContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ShowHistoryListAction extends ActionSupport {
@@ -19,11 +15,7 @@ public class ShowHistoryListAction extends ActionSupport {
   public String execute() throws IOException {
     historyDirList = new HashMap<String, String>();
 
-    String confPath =
-        ServletActionContext.getServletContext().getRealPath(File.separator)
-            + "/../../../app.sparkpowermeter/conf.properties";
-    Properties p = Util.buildProperties(confPath);
-    String backupPath = p.getProperty("workload.output.path");
+    String backupPath = WebCenterContext.getConf().get("workload.output.path");
 
     File root = new File(backupPath);
     File[] files = root.listFiles();
