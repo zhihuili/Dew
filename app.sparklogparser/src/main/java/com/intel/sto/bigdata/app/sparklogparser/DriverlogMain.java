58a60cbed966adaf5aac0e28a7fdd197cf150d10
Li Zhihui <zhihui.li@intel.com>
2015-02-02 15:05:32 +0800

    remove useless comments

--- app.sparklogparser/src/main/java/com/intel/sto/bigdata/app/sparklogparser/DriverlogMain.java
+++ app.sparklogparser/src/main/java/com/intel/sto/bigdata/app/sparklogparser/DriverlogMain.java
@@ -49,7 +49,6 @@ public class DriverlogMain {
         try {
           br.close();
         } catch (IOException e) {
-          // TODO Auto-generated catch block
           e.printStackTrace();
         }
       }
