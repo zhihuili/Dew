99efcb9d62875b27c78c7e1d9064d9124dc0d70a
Li Zhihui <zhihui.li@intel.com>
2015-03-09 09:53:33 +0800

    conf git url

--- app.asp/src/main/java/com/intel/sto/bigdata/app/asp/chart/Git.java
+++ app.asp/src/main/java/com/intel/sto/bigdata/app/asp/chart/Git.java
@@ -6,18 +6,19 @@ import com.intel.sto.bigdata.app.asp.util.Util;
 
 public class Git {
   public static void push(Properties conf) throws Exception {
-    String path =  conf.getProperty("imagedir")+ "../";
-    Util.execute("git pull https://yanqinghuang:1991hyq@github.com/yanqinghuang/Asp.git gh-pages",null ,path);
+    String path = conf.getProperty("imagedir") + "../";
+    String giturl = conf.getProperty("giturl");
+    Util.execute("git pull " + giturl + " gh-pages", null, path);
     Util.execute("git add index.html", null, path);
     Util.execute("git add result.html", null, path);
     Util.execute("git add result_time.html", null, path);
     Util.execute("git add result_release.html", null, path);
     Util.execute("git add about.html", null, path);
     Util.execute("git add plaf1.html", null, path);
-//  Util.execute("git add css", null, path);
+    // Util.execute("git add css", null, path);
     Util.execute("git add image", null, path);
     Util.execute("git commit -m commit", null, path);
-//    Util.execute("git push origin gh-pages", null, path);
-    Util.execute("git push https://yanqinghuang:1991hyq@github.com/yanqinghuang/Asp.git",null ,path);
-    }
+    // Util.execute("git push origin gh-pages", null, path);
+    Util.execute("git push " + giturl, null, path);
+  }
 }
