e6794cec1801dae78a2acefc7f186add07482a72
Li Zhihui <zhihui.li@intel.com>
2015-03-25 16:34:27 +0800

    If tasks number is large, don't output data to task.csv because JFreeChart will be crashed.

--- app.sparklogparser/src/main/java/com/intel/sto/bigdata/app/sparklogparser/util/NodePrinter.java
+++ app.sparklogparser/src/main/java/com/intel/sto/bigdata/app/sparklogparser/util/NodePrinter.java
@@ -13,21 +13,37 @@ import com.intel.sto.bigdata.app.sparklogparser.model.TaskSet;
 public class NodePrinter {
 
   public static void print(App app, String path) throws Exception {
-    FileOutputStream jobFs = new FileOutputStream(new File(path , "job.csv"));
-    FileOutputStream stageFs = new FileOutputStream(new File(path , "stage.csv"));
-    FileOutputStream taskFs = new FileOutputStream(new File(path , "task.csv"));
+    FileOutputStream jobFs = new FileOutputStream(new File(path, "job.csv"));
+    FileOutputStream stageFs = new FileOutputStream(new File(path, "stage.csv"));
+    FileOutputStream taskFs = new FileOutputStream(new File(path, "task.csv"));
 
+    int taskCount = 0;
+    boolean doOutTaskFile = true;
+    for (Job job : app.getChildren()) {
+      for (Stage stage : job.getChildren()) {
+        for (TaskSet taskSet : stage.getChildren()) {
+          for (Task task : taskSet.getChildren()) {
+            taskCount++;
+          }
+        }
+      }
+    }
+    if (taskCount > 100 * 1000) { // JFreeChart will be crashed if data size is large.
+      doOutTaskFile = false;
+    }
+    
     for (Job job : app.getChildren()) {
       String jobName = job.getName();
       jobFs.write((jobName + "," + buildTimeStr(job) + "\r\n").getBytes());
       for (Stage stage : job.getChildren()) {
         String stageName = stage.getName();
         stageFs.write((stageName + "," + jobName + "," + buildTimeStr(stage) + "\r\n").getBytes());
-        for (TaskSet taskSet : stage.getChildren()) {
-          for (Task task : taskSet.getChildren()) {
-            taskFs
-                .write((task.getId() + "," + stageName + "," + jobName + "," + buildTimeStr(task) + "\r\n")
-                    .getBytes());
+        if (doOutTaskFile) {
+          for (TaskSet taskSet : stage.getChildren()) {
+            for (Task task : taskSet.getChildren()) {
+              taskFs.write((task.getId() + "," + stageName + "," + jobName + ","
+                  + buildTimeStr(task) + "\r\n").getBytes());
+            }
           }
         }
       }
