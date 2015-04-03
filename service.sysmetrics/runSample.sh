4d0d05f937ed03f89f5898e1348aea8d8068b9b1
Zhihui Li <zhihui.li@intel.com>
2015-04-03 10:19:03 +0800

    Update runSample.sh

--- service.sysmetrics/runSample.sh
+++ service.sysmetrics/runSample.sh
@@ -2,5 +2,5 @@ cur="`dirname "$0"`"
 cur="`cd "$cur"; pwd`"
 export DEW_HOME=${cur}/..
 classpath="$DEW_HOME/dew.assembly/dew.jar"
-java -cp $classpath com.intel.sto.bigdata.dew.service.sysmetrics.cli.ExampleMain $HOSTNAME:6066
+java -cp $classpath com.intel.sto.bigdata.dew.service.sysmetrics.cli.ExampleMain $HOSTNAME:6766
 
